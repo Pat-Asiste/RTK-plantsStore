478c0294efdfc62050d4531af39f851919ab4fed
Pat-Asiste <barrantespatrick@gmail.com>
2026-04-21 18:05:37 -0500

    RTK backend -- SLICE 'CartSlice.jsx'

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,22 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+      const itemToAdd = state.items.find((planta) => (planta.name === action.payload.name));  // return "1 solo" elemento
+      if (!itemToAdd) { state.items.push({ ...action.payload, quantity: 1 }); }               // If, la planta está repetida, no hagas nada
+      else { itemToAdd.quantity++; }                                                          // buenas prácticas
+
 
     },
     removeItem: (state, action) => {
+      const newState = state.items.filter((planta) => (planta.name !== action.payload));
+      state.items = newState;
     },
     updateQuantity: (state, action) => {
-
-
+      const { name, newQuantity } = action.payload;
+      const ItemToUpdate = state.items.find((planta) => (planta.name === name));
+      if (ItemToUpdate) {
+        ItemToUpdate.quantity = parseInt(newQuantity);
+      };
     },
   },
 });
